d8c77dae2953ec675d9f7e5961922072aad475e2
BruceDai <feng.dai@intel.com>
2020-10-15 18:20:16 +0800

    [tool] Test with ADD(fused relu)/CONV_2D(fused relu).

--- test/cts/tool/CTSConverter/output/cts/specs/V1_0/add.js
+++ test/cts/tool/CTSConverter/output/cts/specs/V1_0/add.js
@@ -3,10 +3,11 @@ describe('CTS', function() {
   const nn = navigator.ml.getNeuralNetworkContext();
   const builder = nn.createModelBuilder();
 
-  it('add test converted from Android NNAPI CTS-ADD test', async function() {
+  it('add + relu test converted from Android NNAPI CTS-ADD test', async function() {
     const op1 = builder.input('op1', {type: 'float32', dimensions: [2]});
     const op2 = builder.input('op2', {type: 'float32', dimensions: [2]});
-    const op3 = builder.add(op1, op2);
+    const intermediateOutput = builder.add(op1, op2);
+    const op3 = builder.relu(intermediateOutput);
     const model = await builder.createModel({'op3', op3});
     const compilation = await model.compile({powerPreference: 'low-power'});
     const op1Buffer = new Float32Array([1.0, 2.0]);
--- test/cts/tool/CTSConverter/output/cts/specs/V1_0/conv_float_channels.js
+++ test/cts/tool/CTSConverter/output/cts/specs/V1_0/conv_float_channels.js
@@ -3,14 +3,15 @@ describe('CTS', function() {
   const nn = navigator.ml.getNeuralNetworkContext();
   const builder = nn.createModelBuilder();
 
-  it('conv2d + add test converted from Android NNAPI CTS-CONV_2D (float channels) test', async function() {
+  it('conv2d + add + relu test converted from Android NNAPI CTS-CONV_2D (float channels) test', async function() {
     const op1 = builder.input('op1', {type: 'float32', dimensions: [1, 1, 1, 3]});
     const op2 = builder.constant({type: 'float32', dimensions: [1, 1, 3, 3]}, new Float32Array([1.0, 2.0, 3.0, 1.0, 2.0, 3.0, 1.0, 2.0, 3.0]));
     const op3 = builder.constant({type: 'float32', dimensions: [3]}, new Float32Array([0.0, 0.0, 0.0]));
     const pad0 = 0;
     const stride = 1;
     const intermediateOutput1 = nn.conv2d(op1, op2, [pad0, pad0, pad0, pad0], [stride, stride], [1, 1], 1, 'nhwc');
-    const op4 = builder.add(intermediateOutput1, op3);
+    const intermediateOutput2 = builder.add(intermediateOutput1, op3);
+    const op4 = builder.relu(intermediateOutput2);
     const model = await builder.createModel({'op4', op4});
     const compilation = await model.compile({powerPreference: 'low-power'});
     const op1Buffer = new Float32Array([99.0, 99.0, 99.0]);
